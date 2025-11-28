f3830fb569d0caab0c2e86921c0dd38e93b87018
Sudeep Patel <sudeeppatel@microsoft.com>
2025-11-28 19:19:06 +0530

    Terraform: Addition container

--- main.tf
+++ main.tf
@@ -35,3 +35,10 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+} 
